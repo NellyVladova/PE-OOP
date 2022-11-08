2462feb657b9ab98666c534a939384707db57cb3
Nelly Vladova <101873582+NellyVladova@users.noreply.github.com>
2022-11-08 23:55:35 +0200

    add car and truck task 2 updated version

--- main.cpp
+++ main.cpp
@@ -1,19 +1,77 @@
+//
+// Created by nelly on 06.11.22.
+//
+
 #include <iostream>
+
 using namespace std;
 
+class truck;
+
+//Предварителна декларация
+class car {
+    int passengers;
+    int speed;
+public:
+    car(int p, int s) {
+        passengers = p;
+        speed = s;
+    }
+
+    friend int sp_greater(car c, truck t);
+
+    friend double calculateTime(car car, int path);
+};
+
+class truck {
+    int weight;
+    int speed;
+public:
+    truck(int w, int s) {
+        weight = w;
+        speed = s;
+    }
+
+    friend int sp_greater(car c, truck t);
+
+    friend double calculateTime(car car, int path);
+};
+
+int sp_greater(car c, truck t) {
+    return c.speed - t.speed;
+}
+
+double calculateTime(car car, int path) {
+    return (path / car.speed);
+}
+
 int main() {
-    int firstNumber, secondNumber;
-    cout << "First number: " << endl;
-    cin >> firstNumber;
-    cout << "Second number: " << endl;
-    cin >> secondNumber;
+    int t;
+    car c1(6, 55), c2(2, 120);
+    truck t1(10000, 55), t2(20000, 72);
+    cout << "Comparing c1 and t1:\n";
+    t = sp_greater(c1, t1);
 
-    firstNumber = firstNumber * secondNumber;
-    secondNumber = firstNumber / secondNumber;
-    firstNumber = firstNumber / secondNumber;
-
-    cout << "First number (new value): " << firstNumber << endl;
-    cout << "Second number (new value): " << secondNumber << endl;
+    if (t < 0) {
+        cout << "Truck is faster.\n";
+    } else if (t == 0) {
+        cout << "Car and truck speed is the same.\n";
+    } else {
+        cout << "Car is faster.\n";
+    }
+    cout << "\nComparing c2 and t2:\n";
+    t = sp_greater(c2, t2);
+    if (t < 0) {
+        cout << "Truck is faster.\n";
+    } else if (t == 0) {
+        cout << "Car and truck speed is the same.\n";
+    } else {
+        cout << "Car is faster.\n";
+    }
+    double time = calculateTime(c1, 3000);
+    cout << "\nTime: " << time << "h" << endl;
 
     return 0;
 }
+
+
